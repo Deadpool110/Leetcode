1396efc1814120678216020ee2ce439cc6add897
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-06-08 09:38:21 +0800

    第1049题 动态规划

---
+++ training/src/leetcode/_1049_lastStoneWeightII.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+public class _1049_lastStoneWeightII {
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+        for (int stone : stones) {
+            sum += stone;
+        }
+
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        for (int stone : stones) {
+            for (int i = target; i >= stone; i--) {
+                dp[i] = Math.max(dp[i], dp[i - stone] + stone);
+            }
+        }
+        return sum - dp[target] * 2;
+    }
+
+//    动态规划
+    public int lastStoneWeightII1(int[] stones) {
+        int sum = 0;
+        for (int weight : stones) {
+            sum += weight;
+        }
+        int m = sum / 2;
+        boolean[] dp = new boolean[m + 1];
+        dp[0] = true;
+        for (int weight : stones) {
+            for (int j = m; j >= weight; --j) {
+                dp[j] = dp[j] || dp[j - weight];
+            }
+        }
+        for (int j = m;; --j) {
+            if (dp[j]) {
+                return sum - 2 * j;
+            }
+        }
+    }
+}
